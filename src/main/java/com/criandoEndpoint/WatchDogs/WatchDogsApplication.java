c11797903b8841adceda1be9cd98cdf480847843
unknown <philippediniz@outlook.com>
2023-10-21 00:22:53 -0300

    first commit

---
+++ src/main/java/com/criandoEndpoint/WatchDogs/WatchDogsApplication.java
@@ -0,0 +1,13 @@
+package com.criandoEndpoint.WatchDogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchDogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchDogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/criandoEndpoint/WatchDogs/WatchDogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.criandoEndpoint.WatchDogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchDogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
